4ccb1f33ee62d06fe3c26def4ee3026825957ae3
JonAmparo <jonathanmamparo@gmail.com>
2020-02-16 17:06:58 -0500

    Updates

---
+++ precache-manifest.c20c63b9145f6b6afac541bb356b1e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab94c2d583e16ef293dab7298ddadab7",
+    "url": "/react-commercejs/index/index.html"
+  },
+  {
+    "revision": "46a5112e5a807ee32eb1",
+    "url": "/react-commercejs/index/static/css/main.4244e52b.chunk.css"
+  },
+  {
+    "revision": "4efc84806903ab9fa309",
+    "url": "/react-commercejs/index/static/js/2.47361987.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-commercejs/index/static/js/2.47361987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a5112e5a807ee32eb1",
+    "url": "/react-commercejs/index/static/js/main.002e77c9.chunk.js"
+  },
+  {
+    "revision": "93dbde5755e68a8dce89",
+    "url": "/react-commercejs/index/static/js/runtime-main.9652a964.js"
+  }
+]);
